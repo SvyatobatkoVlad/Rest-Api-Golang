a601989032cc692dcdfcb9d69b66cafacf149169
Vlad Sviatobatko <vlad@Vlads-MacBook-Pro.local>
2022-11-08 12:27:43 +0200

    added handler but need impl

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	server "github.com/SvyatobatkoVlad/Rest-Api-Golang"
+	handler "github.com/SvyatobatkoVlad/Rest-Api-Golang/pkg/handler"
 	"log"
 )
 
 func main() {
+	handlers := new(handler.Handler)
+
 	srv := new(server.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error running http server: %s", err.Error())
 	}
-
 }
