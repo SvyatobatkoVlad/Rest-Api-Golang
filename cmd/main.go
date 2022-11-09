60e92550b8ff216bf6e906d1a985ef1072bd1278
Vlad Sviatobatko <vlad@Vlads-MacBook-Pro.local>
2022-11-09 17:26:54 +0200

    added clean code

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	server "github.com/SvyatobatkoVlad/Rest-Api-Golang"
 	handler "github.com/SvyatobatkoVlad/Rest-Api-Golang/pkg/handler"
+	"github.com/SvyatobatkoVlad/Rest-Api-Golang/pkg/repository"
+	"github.com/SvyatobatkoVlad/Rest-Api-Golang/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(server.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
